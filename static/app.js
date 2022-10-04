4fadb534f876b8c97aea520ea3e96d00e649c5b7
SupachotsV2 <109055407+SupachotsV2@users.noreply.github.com>
2022-10-04 08:29:51 +0700

    Update

--- static/app.js
+++ static/app.js
@@ -1,55 +1,27 @@
 function getStatusValve(){
-    let url = "https://smartpumptest.herokuapp.com/StatusInfo/";
+    let url = "http://127.0.0.1:8000/StatusInfo/";
         fetch(url)
           .then((res) => res.json())
             .then(function(data){
             let list = data;
             // console.log(list[0]['status'])
-            if(document.getElementById("v1").innerText != list[0]['status'] ||document.getElementById("v2").innerText != list[1]['status'] ||document.getElementById("v3").innerText != list[2]['status'] ||document.getElementById("v4").innerText != list[3]['status']){
               document.getElementById("v1").innerHTML= list[0]['status']
               document.getElementById("v2").innerHTML= list[1]['status']
               document.getElementById("v3").innerHTML= list[2]['status']
               document.getElementById("v4").innerHTML= list[3]['status']
               if(list[0]['status'] == "Off" && list[1]['status'] == "Off" && list[2]['status'] == "Off" && list[3]['status'] == "Off"){
                 changeStatusPump(1,"Off")
-                getStatusPump()
+                document.getElementById("p1").innerHTML= list[0]['status']
               }else{
                 changeStatusPump(1,"On")
-                getStatusPump();
+                document.getElementById("p1").innerHTML= list[0]['status']
               }
-            }else{
-              getStatusPump(); // endless loop
               // lastgetinfovalveandpump();
               // document.getElementById("p1").innerHTML = document.getElementById("p1").innerText
             }
             // console.log( document.getElementById("v4").innerText)
-        }
 )};
 
-// function lastgetinfovalveandpump(){
-//   let url = "http://127.0.0.1:8000/StatusInfo/";
-//     fetch(url)
-//       .then((res) => res.json())
-//         .then(function(data){
-//           document.getElementById("v1").innerHTML= list[0]['status']
-//           document.getElementById("v2").innerHTML= list[1]['status']
-//           document.getElementById("v3").innerHTML= list[2]['status']
-//           document.getElementById("v4").innerHTML= list[3]['status']
-//           document.getElementById("p1").innerHTML = document.getElementById("p1").innerText
-//         })
-// }
-
-function getStatusPump(){
-  let url = "https://smartpumptest.herokuapp.com/WaterPump/"
-    fetch(url)
-      .then((res) => res.json())
-        .then(function(data){
-          let list = data;
-          document.getElementById("p1").innerHTML= list[0]['status']
-          getStatusValve()
-           
-        })
-}
 
 function getCookie(name) {
     let cookieValue = null;
@@ -71,12 +43,12 @@ const csrftoken = getCookie("csrftoken");
 
 function changeStatusValve(task){
     valve_id = task
-    let url = `https://smartpumptest.herokuapp.com/StatusInfo/${valve_id}/`;
+    let url = `http://127.0.0.1:8000/StatusInfo/${valve_id}/`;
     fetch(url)
       .then((res) => res.json())
         .then(function(data){
         let list = data;
-        // console.log(list['status'])
+        console.log(list['status'])
         let status = "On"
         if (list['status'] == "Off"){
             status = "On"
@@ -84,7 +56,7 @@ function changeStatusValve(task){
         else{
             status = "Off"
         }
-        let url = `https://smartpumptest.herokuapp.com/StatusInfo/${valve_id}/`;
+        let url = `http://127.0.0.1:8000/StatusInfo/${valve_id}/`;
         fetch(url,{
         method:"PUT",
         headers:{
@@ -100,7 +72,7 @@ function changeStatusValve(task){
 
 function changeStatusPump(task,status){
     Pump_id = task
-    let url = `https://smartpumptest.herokuapp.com/WaterPump/${Pump_id}/`;
+    let url = `http://127.0.0.1:8000/WaterPump/${Pump_id}/`;
     fetch(url,{
     method:"PUT",
     headers:{
