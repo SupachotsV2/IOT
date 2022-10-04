262011adcb3632e8d4114017a17e6af5b0a05928
SupachotsV2 <109055407+SupachotsV2@users.noreply.github.com>
2022-10-04 08:45:30 +0700

    Update

--- static/app.js
+++ static/app.js
@@ -1,80 +1,82 @@
 function getStatusValve(){
-  let url = "https://smartpumptest.herokuapp.com/StatusInfo/";
-      fetch(url)
-        .then((res) => res.json())
-          .then(function(data){
-          let list = data;
-          // console.log(list[0]['status'])
-          document.getElementById("v1").innerHTML= list[0]['status']
-          document.getElementById("v2").innerHTML= list[1]['status']
-          document.getElementById("v3").innerHTML= list[2]['status']
-          document.getElementById("v4").innerHTML= list[3]['status']
-          if(list[0]['status'] == "Off" && list[1]['status'] == "Off" && list[2]['status'] == "Off" && list[3]['status'] == "Off"){
-            changeStatusPump(1,"Off")
-            document.getElementById("p1").innerHTML= "Off"
-          }else{
-            changeStatusPump(1,"On")
-            document.getElementById("p1").innerHTML= "On"
-          }
-      }
+    let url = "https://smartpumptest.herokuapp.com/StatusInfo/";
+        fetch(url)
+          .then((res) => res.json())
+            .then(function(data){
+            let list = data;
+            // console.log(list[0]['status'])
+              document.getElementById("v1").innerHTML= list[0]['status']
+              document.getElementById("v2").innerHTML= list[1]['status']
+              document.getElementById("v3").innerHTML= list[2]['status']
+              document.getElementById("v4").innerHTML= list[3]['status']
+              if(list[0]['status'] == "Off" && list[1]['status'] == "Off" && list[2]['status'] == "Off" && list[3]['status'] == "Off"){
+                changeStatusPump(1,"Off")
+                document.getElementById("p1").innerHTML= "Off"
+              }else{
+                changeStatusPump(1,"On")
+                document.getElementById("p1").innerHTML= "On"
+              }
 )};
 
+
 function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== "") {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === name + "=") {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-             break;
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+               break;
+              }
             }
           }
+      return cookieValue;
         }
-    return cookieValue;
-      }
-      
+        
 const csrftoken = getCookie("csrftoken");
 
 function changeStatusValve(task){
-  valve_id = task
-  let url = `https://smartpumptest.herokuapp.com/StatusInfo/${valve_id}/`;
-  fetch(url)
-    .then((res) => res.json())
-      .then(function(data){
-      let list = data;
-      console.log(list['status'])
-      let status = "On"
-      if (list['status'] == "Off"){
-          status = "On"
-      }
-      else{
-          status = "Off"
-      }
-      let url = `https://smartpumptest.herokuapp.com/StatusInfo/${valve_id}/`;
-      fetch(url,{
-      method:"PUT",
-      headers:{
-          "Content-type" : "application/json",
-          "X-CSRFToken" : csrftoken,
-      },
-      body:JSON.stringify({status: status}),
-      }).then(function(){
-        getStatusValve()
-      });
-  }
+    valve_id = task
+    let url = `https://smartpumptest.herokuapp.com/StatusInfo/${valve_id}/`;
+    fetch(url)
+      .then((res) => res.json())
+        .then(function(data){
+        let list = data;
+        console.log(list['status'])
+        let status = "On"
+        if (list['status'] == "Off"){
+            status = "On"
+        }
+        else{
+            status = "Off"
+        }
+        let url = `https://smartpumptest.herokuapp.com/StatusInfo/${valve_id}/`;
+        fetch(url,{
+        method:"PUT",
+        headers:{
+            "Content-type" : "application/json",
+            "X-CSRFToken" : csrftoken,
+        },
+        body:JSON.stringify({status: status}),
+        }).then(function(){
+          getStatusValve()
+        });
+    }
 )}
 
 function changeStatusPump(task,status){
-  Pump_id = task
-  url = `https://smartpumptest.herokuapp.com/WaterPump/${Pump_id}/`;
-  fetch(url,{
-  method:"PUT",
-  headers:{
-      "Content-type" : "application/json",
-      "X-CSRFToken" : csrftoken,
-  },
-  body:JSON.stringify({status: status}),
-  })
-}
+    Pump_id = task
+    let url = `https://smartpumptest.herokuapp.com/WaterPump/${Pump_id}/`;
+    fetch(url,{
+    method:"PUT",
+    headers:{
+        "Content-type" : "application/json",
+        "X-CSRFToken" : csrftoken,
+    },
+    body:JSON.stringify({status: status}),
+    })
+  }
+
+
