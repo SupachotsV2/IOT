08501f1f2945aea85afeb7dc4f9d1a5e7561e3f9
SupachotsV2 <109055407+SupachotsV2@users.noreply.github.com>
2022-10-10 13:24:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ origins = [
     "http://localhost:8080",
     "http://localhost:5500",
     "https://stackpython.co",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:8000"
 ]
 
 app.add_middleware(
